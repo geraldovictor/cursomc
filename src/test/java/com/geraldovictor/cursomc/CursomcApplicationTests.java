97fc67afd87820cf00abce6dbfeec3929bcb58e8
geraldovictor <geraldovictor@outlook.com>
2020-02-01 23:56:06 -0300

    Projeto criado

---
+++ src/test/java/com/geraldovictor/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geraldovictor.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

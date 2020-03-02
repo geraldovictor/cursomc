dda5ba92195fec066ab5f06183e0b14ee322e189
geraldovictor <geraldovictor@outlook.com>
2020-03-02 00:02:27 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/geraldovictor/cursomc/CursomcApplication.java
+++ src/main/java/com/geraldovictor/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.geraldovictor.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.geraldovictor.cursomc.domain.Cidade;
 import com.geraldovictor.cursomc.domain.Cliente;
 import com.geraldovictor.cursomc.domain.Endereco;
 import com.geraldovictor.cursomc.domain.Estado;
+import com.geraldovictor.cursomc.domain.Pagamento;
+import com.geraldovictor.cursomc.domain.PagamentoComBoleto;
+import com.geraldovictor.cursomc.domain.PagamentoComCartao;
+import com.geraldovictor.cursomc.domain.Pedido;
 import com.geraldovictor.cursomc.domain.Produto;
+import com.geraldovictor.cursomc.domain.enums.EstadoPagamento;
 import com.geraldovictor.cursomc.domain.enums.TipoCliente;
 import com.geraldovictor.cursomc.repositories.CategoriaRepository;
 import com.geraldovictor.cursomc.repositories.CidadeRepository;
 import com.geraldovictor.cursomc.repositories.ClienteRepository;
 import com.geraldovictor.cursomc.repositories.EnderecoRepository;
 import com.geraldovictor.cursomc.repositories.EstadoRepository;
+import com.geraldovictor.cursomc.repositories.PagamentoRepository;
+import com.geraldovictor.cursomc.repositories.PedidoRepository;
 import com.geraldovictor.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,7 +44,11 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+		
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -55,8 +67,9 @@ public class CursomcApplication implements CommandLineRunner{
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
@@ -77,6 +90,18 @@ public class CursomcApplication implements CommandLineRunner{
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+	
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
@@ -88,6 +113,11 @@ public class CursomcApplication implements CommandLineRunner{
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+	
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
 	}
 
 
--- src/main/java/com/geraldovictor/cursomc/domain/Cliente.java
+++ src/main/java/com/geraldovictor/cursomc/domain/Cliente.java
@@ -37,6 +37,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="Telefone")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();	
+	
 	public Cliente() {
 		
 	}
@@ -90,6 +93,14 @@ public class Cliente implements Serializable{
 		this.tipo =  tipo.getCod();;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+	
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -130,7 +141,7 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
-	
+
 	
 	
 }

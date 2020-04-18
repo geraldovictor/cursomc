563d73b335f4b0f089e4b30a40ecd428932d4e81
geraldovictor <geraldovictor@outlook.com>
2020-04-18 17:18:38 -0300

    Endpoint /pedidos/id disponibilizado

--- src/main/java/com/geraldovictor/cursomc/domain/ItemPedido.java
+++ src/main/java/com/geraldovictor/cursomc/domain/ItemPedido.java
@@ -5,12 +5,16 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId 
 	private ItemPedidoPK id = new ItemPedidoPK();
+	
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
@@ -26,10 +30,6 @@ public class ItemPedido implements Serializable {
 		this.desconto = desconto;
 		this.quantidade = quantidade;
 		this.preco = preco;
-	}
-
-	public Pedido getPedido() {
-		return id.getPedido();
 	}
 	
 	@Override
@@ -56,7 +56,12 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-
+	
+	@JsonIgnore
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
 	public Produto getProduto() {
 		return id.getProduto();
 	}
